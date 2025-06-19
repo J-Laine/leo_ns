6670cdcc0293c4a50da486264d9d1923a0b05cde
Jesperi Laine <115222601+J-Laine@users.noreply.github.com>
2025-06-19 17:24:31 +0300

    Added wordAnimation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,19 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import ClockWordAnimation from "../components/ClockwordAnimation";
 
 export default function Home() {
   return (
     <>
         <div className="bg-green-200 p-8 h-[450px] text-black flex items-center justify-center">
-              Highlighted content with yellow background
+            Highlighted content with yellow background
+             <ClockWordAnimation />
         </div>
         <div className="bg-red-200 p-8 h-[450px] text-black flex items-center justify-center">
-            palveluvaihtoehto karuselli
+          palveluvaihtoehto karuselli
         </div>
         <div className="bg-blue-200 p-8 h-[650px] text-black flex items-center justify-center">
-            yhetydenottolomake jne
+          yhetydenottolomake jne
         </div>
     </>
   );
---
+++ src/components/ClockwordAnimation.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import styles from '../css/ClockWordAnimation.module.css'
+
+const sentences = ["Kärsitkö niska-hartijakivuista näyttöpäätetyön takia?", "Oletko urheilija ja haluat optimoida suorituskykysi?", "Oli vaivasi mikä tahansa, teen parhaani sen ratkaisemiseksi!"];
+
+export default function ClockWordAnimation() {
+  const [index, setIndex] = useState(0);
+  const [isAnimating, setIsAnimating] = useState(false);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIsAnimating(true);
+      setTimeout(() => {
+        setIndex((prev) => (prev + 1) % sentences.length);
+        setIsAnimating(false);
+      }, 300); // Matches the animation duration
+    }, 4000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <h1 className="font-semibold text-2xl md:text-3xl">
+      kärsitkö
+      <span className={styles.wordWrapper}>
+        <span 
+          className={`${styles.rotatingWord} ${isAnimating ? styles.animateOut : styles.animateIn}`}
+        >
+          {sentences[index]}
+        </span>
+      </span>
+    </h1>
+  );
+}
